5931c86163a974f17993fcff1363ac79efe0df32
JoacoMaurtua <joacomaurtua@gmail.com>
2021-08-31 12:47:48 -0500

    error solved

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -2,5 +2,14 @@
 
 const User = require('../models/user.models'); //importando Users
 
-const findUser
+const findUser = (req, res) => {
+    User.find({})
+        .then(result => res.json({data:result})) //trae un arreglo de objetos
+        .catch(error => {
+            res.json({error: error, message: "Users not founded"});
+            res.sendStatus(404); //error http
+        })
+}
+
+module.exports = {findUser}; //exportar todas las funcionalidades
 
