fd02ebbeeef33748f94c7855dad1ed5b11d1b005
JoacoMaurtua <joacomaurtua@gmail.com>
2021-08-30 23:14:35 -0500

    firts schema created

--- server/models/user.models.js
+++ server/models/user.models.js
@@ -1,2 +1,37 @@
 //Creamos nuestro esquema(campos)
 
+const mongoose = require('mongoose');
+
+//Creamos nuestro esquema
+
+const UserSchema = new mongoose.Schema({
+  nickName:{
+    type: String,
+    required:[true,'Nick Name required!']
+  },
+
+  fullName:{
+    type: String,
+    required:[true,'Full Name required!']
+  },
+
+  email:{
+    type: String,
+    required:[true,'Email required!'],
+    validate:{
+      validator:val => /^([\w-.]+@([\w-]+.)+[\w-]+)?$/.test(val), //formato correcto del correo --> https://regexr.com/95714
+      message:'Please type a correct format for the email!'
+    }
+  },
+
+  password:{
+    type: String,
+    required:[true,'Password required!'],
+    minlength:[8,'The password must be great than 8 characters']
+  },
+
+},{timestamps:true}) //timestamps es para saber la fecha y hora en la que se crea un nuevo user
+
+const User = mongoose.model('Users',UserSchema);
+
+module.exports = User;
